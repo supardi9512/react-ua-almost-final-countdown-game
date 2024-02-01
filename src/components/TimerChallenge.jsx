0239a3e20477d87211f33b24017795bce8ca18fb
supardi_9512 <supardi@atomic.id>
2024-02-01 22:31:22 +0700

    managing stop challenge state using refs

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({title, targetTime}) {
+    const timer = useRef();
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
 
     function handleStart() {        
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
 
         setTimerStarted(true);
+    }
+
+    function handleStop() {
+        clearTimeout(timer.current);
     }
 
     return (
@@ -20,7 +25,7 @@ export default function TimerChallenge({title, targetTime}) {
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button onClick={handleStart}>
+                <button onClick={timerStarted ? handleStop : handleStart}>
                     {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
