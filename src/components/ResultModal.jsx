f7fd497c17a72a96e6be600fbe2192105dbdda8d
supardi_9512 <supardi@atomic.id>
2024-02-09 23:08:12 +0700

    reset timer after closing modal via the escape key

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -18,7 +18,7 @@ const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, 
     });
 
     return (
-        <dialog ref={dialog} className="result-modal">
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You lost</h2>}
             {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
